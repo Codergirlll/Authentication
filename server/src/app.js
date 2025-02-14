d90430cf56c441fa94c8e421bdc606da2f3ef9c3
Himani Gangwar <himani.gangwar@w2c.co.in>
2025-02-14 17:21:19 +0530

    Incomplete Cookies functionlity

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express");
 const app = express();
 require("dotenv").config();
@@ -9,19 +7,22 @@ const cookieParser = require("cookie-parser");
 
 // for app requirement
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
 
+// Updated CORS configuration to allow credentials
+// app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from this origin
+    credentials: true, // Allow cookies to be sent
+}));
 
 // for Importing Database
 require("./db/connection");
 
-
 // for Importing Routers
 app.use("/api/v1", UserRouter);
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running at port no. ${port}`);
-})
+});
--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -3,13 +3,16 @@ const jwt = require("jsonwebtoken");
 
 exports.Auth = async (req, res, next) => {
     try {
-        const authHeader = req.headers.authorization;
+        // const authHeader = req.headers.authorization;
+        console.log("req: ", req.cookies)
+        const token = req.cookies.token;
+        // console.log("authHeader: ", authHeader)
+        // console.log("authCookie: ", authHeader)
+        // if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        //     return res.status(401).json({ message: "Unauthorized: No token provided" });
+        // }
 
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            return res.status(401).json({ message: "Unauthorized: No token provided" });
-        }
-
-        const token = authHeader.split(" ")[1]; // Extract token
+        // const token = authHeader.split(" ")[1]; // Extract token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         console.log("decoded: ", decoded)
